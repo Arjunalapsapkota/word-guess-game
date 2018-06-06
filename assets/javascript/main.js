fa89c8350021981c942f62448fb81baddb5ac002
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-06-06 09:21:49 -0700

    picture-renamed

---
+++ assets/javascript/main.js
@@ -0,0 +1,30 @@
+//10 songs, Acutal words user has to make a guess 
+const songs=["legends",
+             "bad liar",
+             "bodak yellow",
+             "chanel","Despacito",
+             "new rules", 
+             "humble",
+             "green light",
+             "imagine",
+             "money"];
+             
+//10 corresponding artist
+
+const artist=["Kelsea Ballerini",
+             "Selena Gomez",
+             "Cardi B",
+             "Frank Ocean",
+             "Luis Fosi",
+             "Dua Lipa",
+             "Kendrik Lamar", 
+             "Lorde",
+             "John Lenon",
+             "Pink Floyd"];
+
+function number(){
+    //var counter=Math.floor (Math.random() *100000000);
+    return math.floor(math.random()*10+1);
+}
+//selects the corresponding divs
+var clue = $("#clue-box");
