d978f5533cf49ece73be0911158e428830405adf
arjunalapsapkota <arjunalapsapkota@gmail.com>
2018-06-06 21:55:56 -0700

    clue-box

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,5 @@
+
+$(document).ready(function(){
 //10 songs, Acutal words user has to make a guess 
 const songs=["legends",
              "bad liar",
@@ -15,16 +17,35 @@ const artist=["Kelsea Ballerini",
              "Selena Gomez",
              "Cardi B",
              "Frank Ocean",
-             "Luis Fosi",
+             "Luis Fonsi",
              "Dua Lipa",
              "Kendrik Lamar", 
              "Lorde",
              "John Lenon",
              "Pink Floyd"];
-
+$(".play").on("click", function() {
+                
+               
 function number(){
     //var counter=Math.floor (Math.random() *100000000);
-    return math.floor(math.random()*10+1);
+    return Math.floor(Math.random()*10);
 }
 //selects the corresponding divs
 var clue = $("#clue-box");
+var number= number();
+console.log(number);
+var artistImage="assets/images/"+number+".jpg";
+console.log(artistImage);
+//------------------
+//creating a var "image" which represents <img>
+var image = $("<img>");
+//-------------------
+//****************
+//var image="<img src=\"assets/images/1.jpg\"></img>";
+image.attr("src",artistImage);
+image.addClass("image-clue");
+//image.src("assets/images/1.jpg");
+clue.replaceWith(image);
+
+});    
+});
